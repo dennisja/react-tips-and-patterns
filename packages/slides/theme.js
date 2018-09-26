65a0ba40b13b7673a8dc5a4a45e93aaf7497b4d1
dennisja <dennisjjagwe@gmail.com>
2018-09-26 12:46:12 +0300

    Put all slides in one file to import theme Sanitize first slide tips

--- packages/slides/theme.js
+++ packages/slides/theme.js
@@ -1,11 +1,18 @@
 import theme from 'mdx-deck/themes';
+import okaidia from 'react-syntax-highlighter/styles/prism/okaidia';
 
 export default {
   ...theme,
-
-  blockquote: {
-    fontSize: '0.1rem',
-    fontStyle: 'italic',
-    color: 'gray',
+  colors: {
+    text: 'rgb(106, 215, 152)',
+    background: 'rgb(1, 22, 39)',
+    link: 'blue',
+  },
+  prism: {
+    style: okaidia,
+  },
+  strong: {
+    color: 'yellow',
+    textTransform: 'capitalize',
   },
 };
